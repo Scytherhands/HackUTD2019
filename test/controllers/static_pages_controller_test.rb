1f155123093bdf9b9452c021011b74314b8b003d
Katerina <katerina.boyer@tamu.edu>
2019-02-07 00:12:21 -0600

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,31 @@
 require 'test_helper'
 
+def setup
+  @base_title = "Twelve TAMU"
+end
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "should get root" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get home" do
+    get static_pages_home_url
     assert_response :success
+    # assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    # assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should_get_contact" do
+    get static_pages_contact_url
+    assert_response :success
+    # assert_select "title", "Contact | #{@base_title}"
   end
 
 end
